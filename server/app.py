f58a34c1d941cfe0a133b9186dc32d08d9c5a960
Ezequiel Tejerina <tejerinae@gmail.com>
2022-06-01 12:41:17 -0300

    change status codes

--- server/app.py
+++ server/app.py
@@ -37,21 +37,21 @@ def get_db():
 def create_user(user: schemas.ClientCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(status_code=409, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
 @app.put("/clients/{user_id}", response_model=schemas.Client)
 def update_user(user_id: int, user: schemas.ClientCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if not db_user:
-        raise HTTPException(status_code=400, detail="User does not exists")
+        raise HTTPException(status_code=204, detail="User does not exists")
     return crud.update_user(db=db, user=user, db_user=db_user)
 
 @app.delete("/clients/{user_id}", response_model=Any)
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if not db_user:
-        raise HTTPException(status_code=400, detail="User does not exists")
+        raise HTTPException(status_code=204, detail="User does not exists")
     return crud.delete_user(db=db, db_user=db_user)
 
 
@@ -65,7 +65,7 @@ def read_clients(skip: int = 0, limit: int = 100, db: Session = Depends(get_db))
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
-        raise HTTPException(status_code=404, detail="Client not found")
+        raise HTTPException(status_code=204, detail="Client not found")
     return db_user
 
 @app.post("/clients/{user_id}/pets/", response_model=schemas.Pet)
