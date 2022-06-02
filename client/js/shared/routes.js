b8ff17bc61f5320cd527550b1638c1c3b497bad9
Emilio Dominguez <emilioadominguezb@gmail.com>
2022-06-02 18:04:17 -0300

    Minor adjustments

--- client/js/shared/routes.js
+++ client/js/shared/routes.js
@@ -1,7 +1,7 @@
 export const routes = Object.freeze([
     { label: "Home", url: "/" },
     { label: "Clients (API)", url: "/pages/clients-api.html" },
-    { label: "Clients (LS)", url: "/pages/clients-ls.html" },
     { label: "Pets (API)", url: "/pages/pets-api.html" },
+    { label: "Clients (LS)", url: "/pages/clients-ls.html" },
     { label: "Pets (LS)", url: "/pages/pets-ls.html" },
 ]);
