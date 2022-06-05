2106da95a821713300c6f83afcfbd07383cf6366
Emilio Dominguez <emilioadominguezb@gmail.com>
2022-06-05 03:22:35 -0300

    Adjusted app routes

--- client/js/shared/routes.js
+++ client/js/shared/routes.js
@@ -2,5 +2,4 @@ export const routes = Object.freeze([
     { label: "Home", url: "/" },
     { label: "Clients", url: "/pages/clients-api.html" },
     { label: "Pets", url: "/pages/pets.html" },
-    { label: "Clients (LS)", url: "/pages/clients-ls.html" },
 ]);
